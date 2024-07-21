b70c17333d78e646c0fff87bc07ce9f6cf16d110
vincilisi <96939594+vincilisi@users.noreply.github.com>
2024-07-21 14:53:40 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+let num1= 14;
+console.log("Il valore di num1 è: " + num1);
+
+
+let num2= 23;
+console.log("Il valore di num2 è: " + num2);
+
+let stringa= 'Fatima';
+console.log("Il valore della stringa è: "+ stringa);
+
+stringa='Vincenzo'
+console.log("Il valore della stringa è: "+ stringa);
+
+const PIGRECO= 3.14159265359;
+console.log("Il valore di PIGRECO è:" + PIGRECO);
+
+PIGRECO= 3.15;
